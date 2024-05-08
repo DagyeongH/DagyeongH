92f3ebec7134a28496463da38753a7d85b29eb1e
Dagyeong <56528107+DagyeongH@users.noreply.github.com>
2024-05-08 23:12:20 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -15,7 +15,20 @@ for idx, feed in enumerate(rss_feed['entries']):
     feed_date = feed['published_parsed']
     latest_blog_post_list += f"[{feed_date.tm_year}.{feed_date.tm_mon}.{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
     
-markdown_text = """기본으로 변하지 않을 README.md 값
+markdown_text = """
+###  :wave: Welcome my github profile :wave:  
+  
+![header](https://capsule-render.vercel.app/api?type=cylinder&color=7193bd&height=150&section=header&text=DagyeongH&fontColor=ffffff&fontSize=70&animation=fadeIn&fontAlignY=55&desc=%20&descAlignY=62&descAlign=62)
+
+####  🔨 Once I've used 🔨
+<a href="링크"><img src="https://img.shields.io/badge/Python-3776AB?style=flat-square&logo=Python&logoColor=white"/></a>
+<a href="링크"><img src="https://img.shields.io/badge/pandas-150458?style=flat-square&logo=pandas&logoColor=white"/></a>
+<a href="링크"><img src="https://img.shields.io/badge/NumPy-013243?style=flat-square&logo=NumPy&logoColor=white"/></a>
+<a href="링크"><img src="https://img.shields.io/badge/MySQL-4479A1?style=flat-square&logo=mysql&logoColor=white"/></a>
+<a href="링크"><img src="https://img.shields.io/badge/jupyter-F37626?style=flat-square&logo=jupyter&logoColor=white"/></a>
+
+
+#### 📓 My log 📓
 """
 
 readme_text = f"{markdown_text}{latest_blog_post_list}"
